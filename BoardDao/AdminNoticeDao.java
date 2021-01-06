83a97e7d8339e65e1ec762870b70c80ac3ccf706
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-06 23:37:45 +0900

    jsp , controller, sqlMapperXml

---
+++ BoardDao/AdminNoticeDao.java
@@ -0,0 +1,23 @@
+package com.exhibition.project.BoardDao;
+
+import java.util.ArrayList;
+
+import com.exhibition.project.BoardDto.AdminNoticeDto;
+
+public interface AdminNoticeDao {
+
+	public void AdminInsert(String boardtopic,String userid,String board_comment);
+
+	public ArrayList<AdminNoticeDto> AdaoPagelist(int itemFirstNum, int itemLastNum);
+
+	public AdminNoticeDto board_view(int board_num);
+
+	public void upHit(int board_num);
+
+	public void Doboard_update(int board_num, String userid, String boardtopic, String board_comment);
+
+	public void Doboard_delete(int board_num);
+	//공지 게시판 최신글 상단에 위치//
+	public String LatestItems();
+
+}
